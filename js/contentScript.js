e1e9bf2887a31ebc2a8ddd708d3134867aba12d1
Andras Katona <akatona@cloudera.com>
2019-06-03 12:17:22 +0200

    allowing special characters such as ,.:() around them

--- js/contentScript.js
+++ js/contentScript.js
@@ -16,7 +16,7 @@
 
 'use strict';
 
-var JIRAFIED = 'jirafied';
+let JIRAFIED = 'jirafied';
 
 // when config changes
 chrome.storage.onChanged.addListener(function(changes, namespace) {
@@ -44,7 +44,7 @@ function loadAndRun(mutationList) {
           }
         }
     }
-    var projectConfigs = [];
+    let projectConfigs = [];
     result.projects.forEach(function(project) {
       projectConfigs.push(createProjectConfig(project.url, project.regex));
     });
@@ -61,7 +61,7 @@ function loadAndRun(mutationList) {
 function createProjectConfig(url, regex) {
   return {
     url: asLink(url),
-    wrappedRegex: new RegExp(`(^|\\s+)${regex}($|\s+)`, 'g'),
+    wrappedRegex: new RegExp(`(^|\\s+|[(])${regex}($|\s+|[,.:;)])`),
     regex: new RegExp(regex, 'g')
   };
 }
@@ -93,7 +93,7 @@ function scanReplaceTextInNode(node, projectConfigs) {
     }).each(function() {
       for (let config of projectConfigs) {
         if (config.wrappedRegex.test(this.data)) {
-          var nodes = $.parseHTML(this.data.replace(config.regex, config.url));
+          let nodes = $.parseHTML(this.data.replace(config.regex, config.url));
           for (let node of nodes) {
             this.before(node);
           }
